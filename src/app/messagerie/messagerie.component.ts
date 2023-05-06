b00b93323fdf7b7ddd9b84a1c3ac4e5c228d80f0
fatima2110 <fatimaamiri2030@gmail.com>
2023-05-06 22:08:01 +0000

    messagerie update

--- src/app/messagerie/messagerie.component.ts
+++ src/app/messagerie/messagerie.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Message } from '../Classes/Message';
 import { Router } from '@angular/router';
 import { MessagerieService } from '../pages/responsbale/Services/messagerie.service';
+import { AuthService } from '../services/AuthService';
+import { LoginService } from '../FournisseurComponents/services/login.service';
 
 @Component({
   selector: 'app-messagerie',
@@ -10,65 +12,81 @@ import { MessagerieService } from '../pages/responsbale/Services/messagerie.serv
 })
 export class MessagerieComponent {
   message: Message[] = [];
-  id:number= 2;
+  id: number = 0;
   m: Message = new Message;
- vue:number=0;
-  constructor( private service:MessagerieService,private router: Router)
-  {
-this.service.getMessage(this.id).subscribe((message: Message[]) => {
-console.log("on va aficher la list")
-  this.message = message;
-  console.log(message)
-});
-this.service.getvue(this.id).subscribe((n:number) => {
-  console.log("on va aficher la list")
-    this.vue = n;
-    console.log(n)
-  });
+  vue: number = 0;
+  constructor(private service: MessagerieService, private router: Router, private auth: AuthService, private serviceFour: LoginService) {
+    const role: string | null = auth.getRole();
+    if (role !== null && role !== undefined) {
+      if (role === "FOURNISSEUR") {
+        this.serviceFour.GetFournisseur().subscribe({
+          next:(res)=>{
+            this.id = res.id;
+          },error:(err)=>{console.log(err)}
+        })
+      }
+      else {
+        const str: string | null = auth.getId();
+        if (str !== null && str !== undefined) {
+          this.id = parseInt(str);
+        }
+      }
+
+      this.service.getMessage(this.id).subscribe((message: Message[]) => {
+        console.log("on va aficher la list des notif de :", this.id)
+        this.message = message;
+        console.log(message)
+      });
+      this.service.getvue(this.id).subscribe((n: number) => {
+        console.log("on va aficher la list ici")
+        this.vue = n;
+        console.log(n)
+      });
+
+    }
 
   }
 
-suprimmer(id:number){
-  console.log("on va suprimmer")
-  alert(id)
-  this.service.getsuprimmer(id).subscribe(() => {
+  suprimmer(id: number) {
+    console.log("on va suprimmer")
+    alert(id)
+    this.service.getsuprimmer(id).subscribe(() => {
 
 
     });
 
-}
-rediriger(m: Message) {
-  let link = "";
-
-  if (m.message == "Nouvelle besoin sont ajouter  pour generer appelle d'offre") {
-    link = "./consulterBesoin";
   }
-   if (m.message == "votre proposition a ete acepte") {
-    if(!m.exsist)
-    {
-      link = "./FournissuerInfo";
+  rediriger(m: Message) {
+    let link = "";
+
+    if (m.message == "Nouvelle besoin sont ajouter  pour generer appelle d'offre") {
+      link = "./consulterBesoin";
     }
+    if (m.message == "accept Proposition") {
+      if (!m.exsist) {
+        link = "./FournissuerInfo";
+      }
 
-   else
-   link = "./consulterBesoin";
+      else
+        link = "./consulterBesoin";
 
+    }
+    else
+      if (m.message == "votre Proposition a ete rejete") {
+        link = "./consulterBesoin";
+      }
+      else
+        if (m.message == "Vous pouvez maintenant ajouter des besoins si vous avez") {
+          link = "./consulterBesoin";
+        }
+        else
+          if (m.message == "Bonjour j'ai ajouter des besoins ") {
+            link = "./consulterBesoin";
+          }
+          else
+            link = "./consulterBesoin";
+    this.router.navigateByUrl(link);
   }
-  else
-  if (m.message == "votre Proposition a ete rejete") {
-    link = "./consulterBesoin";
-  }
-else
-  if (m.message == "Vous pouvez maintenant ajouter des besoins si vous avez") {
-    link = "./consulterBesoin";
-  }
-  else
-  if (m.message == "Bonjour j'ai ajouter des besoins ") {
-    link = "./consulterBesoin";
-  }
- else
-    link = "./consulterBesoin";
-  this.router.navigateByUrl(link);
-}
 
 
 
